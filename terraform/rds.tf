01e5178a245f9e80cda86365c41c4476fa44580e
Justin Reeves <reeves122@gmail.com>
2022-11-08 15:03:31 -0600

    use var.vpc_id

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,7 +1,7 @@
 module "aurora" {
   source                    = "github.com/champ-oss/terraform-aws-aurora.git?ref=45c1ed44506e9b4c2a9006804c2ab06bb5babe41"
   private_subnet_ids        = var.private_subnet_ids
-  vpc_id                    = module.core.vpc_id
+  vpc_id                    = var.vpc_id
   source_security_group_id  = module.core.ecs_app_security_group
   cluster_identifier_prefix = var.git
   database_name             = "grafana"
