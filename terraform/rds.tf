d75e4621470ac052559897640deab4e4a87c59f0
Justin Reeves <reeves122@gmail.com>
2022-11-08 15:00:24 -0600

    fix module reference format

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,5 +1,5 @@
 module "aurora" {
-  source                    = "git::git@github.com:champ-oss/terraform-aws-aurora.git?ref=45c1ed44506e9b4c2a9006804c2ab06bb5babe41"
+  source                    = "github.com/champ-oss/terraform-aws-aurora.git?ref=45c1ed44506e9b4c2a9006804c2ab06bb5babe41"
   private_subnet_ids        = var.private_subnet_ids
   vpc_id                    = module.core.vpc_id
   source_security_group_id  = module.core.ecs_app_security_group
