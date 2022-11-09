e44b840e34512cb47672abc8d64e1975dce418ad
Justin Reeves <reeves122@gmail.com>
2022-11-09 10:58:04 -0600

    update aurora module

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,5 +1,5 @@
 module "aurora" {
-  source                    = "github.com/champ-oss/terraform-aws-aurora.git?ref=45c1ed44506e9b4c2a9006804c2ab06bb5babe41"
+  source                    = "github.com/champ-oss/terraform-aws-aurora.git?ref=93882dab174d257c71e85ccd598cd2e7ea059365"
   private_subnet_ids        = var.private_subnet_ids
   vpc_id                    = var.vpc_id
   source_security_group_id  = module.core.ecs_app_security_group
